eb61b3ec26c6989bd5476d71a4f2cc320e01337d
AartiVerma <AartiVerma@users.noreply.github.com>
2015-06-26 21:32:49 +0530

    Update time.js

--- time.js
+++ time.js
@@ -1,17 +1,32 @@
+var drgId;
+var i=1;
+var b;
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
 function drag(ev) {
+		console.log("DragId::"+ev.target.id);
+		dragId=ev.target.id;	
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-
-    ev.preventDefault();
+	if(dragId=='drag1'||dragId=='drag2'||dragId=='drag3'||dragId=='drag4'||dragId=='drag5'||dragId=='drag6'||dragId=='drag7'||dragId=='drag8')
+	{
+			console.log("match");
+			ev.preventDefault();
+			var data = ev.dataTransfer.getData("text");
+			var nodeCopy = document.getElementById(data).cloneNode(true);
+			nodeCopy.id = 'drop'+i;
+			ev.target.appendChild(nodeCopy);
+		i++;
+	}
+	else{
+		ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-  
-	var nodeCopy = document.getElementById(data).cloneNode(true);
-  nodeCopy.id = "newId"; /* We cannot use the same ID */
-  ev.target.appendChild(nodeCopy);
+    ev.target.appendChild(document.getElementById(data));
+	}		
+	
 }
